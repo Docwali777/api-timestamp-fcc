f82220ac2a9a3f8a4b4b5a62a22a37097bc02dbf
docwali777 <drgauvin@mgmcare.org>
2017-04-15 00:51:49 -0400

    changes to index.ejs and .gitignore

--- server.js
+++ server.js
@@ -4,50 +4,50 @@ const bodyParser = require('body-parser')
 const querystring = require('querystring');
 const URL = require('url').URL;
 
+
+
 const PORT = process.env.PORT || 3000
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true }))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-  res.render('index')
+
+app.get('/', (req, res)=>{
+res.render('index')
 })
-app.get('/:date', (req, res) => {
+app.get('/:date', (req, res)=>{
 
-  let {date} = req.params
+let { date } = req.params
 
-  let dateAndUnix = {
-    UnixTime: Boolean(new Date(date).valueOf()) === false
-      ? + date
-      : new Date(date).valueOf()
-  }
+let dateAndUnix = {
+  unix: Boolean(new Date(date).valueOf()) ===false ? (+date > 1 ? +date : 'Please enter correct Unix Time') : new Date(date).valueOf()
+}
 
-  let setMonth = {
-    1: 'January',
-    2: 'February',
-    3: 'March',
-    4: 'April',
-    5: 'May',
-    6: 'June',
-    7: 'July',
-    8: 'August',
-    9: 'September',
-    10: 'October',
-    11: 'November',
-    12: 'December'
-  }
+let setMonth = {
+  1 : 'January',
+  2 : 'February',
+  3 : 'March',
+  4 : 'April',
+  5 : 'May',
+  6 : 'June',
+  7 : 'July',
+  8 : 'August',
+  9 : 'September',
+  10: 'October',
+  11: 'November',
+  12: 'December'
+}
 
-  let day = (new Date(+ date).getDay())
-  let month = (new Date(+ date).getMonth())
-  let year = new Date(+ date).getFullYear()
-  dateAndUnix.Date = +date !== dateAndUnix.unix
-    ? date
-    : `${setMonth[month]} ${day}, ${year} `
+let day = (new Date(+date).getDay())
+let month = (new Date(+date).getMonth())
+let year = new Date(+date).getFullYear()
+dateAndUnix.date= +date !== dateAndUnix.unix ? (+new Date(date) > 0 ? date : 'Please enter correct Date format') : `${setMonth[month]} ${day}, ${year} `
 
   res.json(dateAndUnix)
 })
 
-app.listen(PORT, () => {
+
+app.listen(PORT, ()=>{
   console.log(`PORT ${PORT}`)
 })
