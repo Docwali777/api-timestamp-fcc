e9004a6ebf0c902ad0de6adc82d28a37df508f62
docwali777 <drgauvin@mgmcare.org>
2017-04-20 13:55:44 -0400

    final touches

--- server.js
+++ server.js
@@ -20,28 +20,39 @@ app.get('/:date', (req, res)=>{
 
 let { date } = req.params
 
+
+
+let setMonth = {
+  0 : 'January',
+  1 : 'February',
+  2 : 'March',
+  3 : 'April',
+  4 : 'May',
+  5 : 'June',
+  6 : 'July',
+  7 : 'August',
+  8 : 'September',
+  9: 'October',
+  10: 'November',
+  11: 'December'
+}
+var d = new Date();
+var weekday = new Array(7);
+weekday[0] =  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+
+let day =(new Date(+req.params.date).getDate())
+let month = (new Date(+date).getMonth())
+let year = new Date(+date).getFullYear()
 let dateAndUnix = {
   unix: Boolean(new Date(date).valueOf()) ===false ? (+date > 1 ? +date : 'Please enter correct Unix Time') : new Date(date).valueOf()
 }
-
-let setMonth = {
-  1 : 'January',
-  2 : 'February',
-  3 : 'March',
-  4 : 'April',
-  5 : 'May',
-  6 : 'June',
-  7 : 'July',
-  8 : 'August',
-  9 : 'September',
-  10: 'October',
-  11: 'November',
-  12: 'December'
-}
-
-let day = (new Date(+date).getDay())
-let month = (new Date(+date).getMonth())
-let year = new Date(+date).getFullYear()
 dateAndUnix.date= +date !== dateAndUnix.unix ? (+new Date(date) > 0 ? date : 'Please enter correct Date format') : `${setMonth[month]} ${day}, ${year} `
 
   res.json(dateAndUnix)
